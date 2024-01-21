7e7498ed5ab920539f0e9a345ae19c3cc8455ad3
Luado <lempb.pessoa@gmail.com>
2024-01-21 19:36:57 -0300

    Last additions
     Changes to be committed:
    	modified:   api/controllers/symbolGenerator.js
    	modified:   api/routes/symbolGenerator.js
    	modified:   package.json
    	new file:   symbol-generator-ui/.editorconfig
    	new file:   symbol-generator-ui/.gitignore
    	new file:   symbol-generator-ui/.vscode/extensions.json
    	new file:   symbol-generator-ui/.vscode/launch.json
    	new file:   symbol-generator-ui/.vscode/tasks.json
    	new file:   symbol-generator-ui/README.md
    	new file:   symbol-generator-ui/angular.json
    	new file:   symbol-generator-ui/package-lock.json
    	new file:   symbol-generator-ui/package.json
    	new file:   symbol-generator-ui/src/app/app.component.html
    	new file:   symbol-generator-ui/src/app/app.component.scss
    	new file:   symbol-generator-ui/src/app/app.component.spec.ts
    	new file:   symbol-generator-ui/src/app/app.component.ts
    	new file:   symbol-generator-ui/src/app/app.config.ts
    	new file:   symbol-generator-ui/src/app/app.routes.ts
    	new file:   symbol-generator-ui/src/assets/.gitkeep
    	new file:   symbol-generator-ui/src/favicon.ico
    	new file:   symbol-generator-ui/src/index.html
    	new file:   symbol-generator-ui/src/main.ts
    	new file:   symbol-generator-ui/src/styles.scss
    	new file:   symbol-generator-ui/tsconfig.app.json
    	new file:   symbol-generator-ui/tsconfig.json
    	new file:   symbol-generator-ui/tsconfig.spec.json

--- api/controllers/symbolGenerator.js
+++ api/controllers/symbolGenerator.js
@@ -2,8 +2,11 @@ module.exports = (app) => {
     const controller = {};
     let sg = require("../../assets/symbolGenerator")
     let ms = require("../../assets/md-simbologia")
-    ms.useMD33(app.get('useMD33'))
-    ms.drawOctagon(app.get('drawOctagon'))
+    ms.useMD33(app.get('useMD33').toLowerCase() == "true")
+    ms.drawOctagon(app.get('drawOctagon').toLowerCase() == "true")
+    console.log('Showing configs inside app');
+    console.log('drawOctagon: ' + app.get("drawOctagon"));
+    console.log('fin');
     controller.symbolGenerator = (req, res) => {
         sidc = req.body.sidc
         options = req.body.options
--- api/routes/symbolGenerator.js
+++ api/routes/symbolGenerator.js
@@ -2,4 +2,6 @@ module.exports = app => {
     const controller = app.controllers.symbolGenerator;
     app.route('/api/v1/gen')
         .post(controller.symbolGenerator);
+    app.route('/api/v1/gen')
+        .get(controller.symbolGenerator);
 }
